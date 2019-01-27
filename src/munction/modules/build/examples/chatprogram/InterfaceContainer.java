a379d9231b2e48a7adb457c534fc110a0e00e25d
Max Rupplin <mearvk@outlook.com>
2019-01-26 20:18:05 -0500

    Munction [latest]

--- src/munction/modules/build/examples/chatprogram/InterfaceContainer.java
+++ src/munction/modules/build/examples/chatprogram/InterfaceContainer.java
@@ -1,10 +1,26 @@
 package munction.modules.build.examples.chatprogram;
 
-import javax.swing.*;
+import munction.modules.build.examples.chatprogram.components.*;
 
 public class InterfaceContainer
 {
-    JFrame jframe;
+    JFrameX jframe;
+
+    JPanelX jpanelA;
+
+    JPanelX jpanelB;
+
+    JMenuBarX jmenubarA;
+
+    JMenuX jmenuA;
+
+    JMenuX jmenuB;
+
+    JButtonX jbuttonA;
+
+    JButtonX jbuttonB;
+
+    JButtonX jbuttonC;
 
     public InterfaceContainer()
     {
