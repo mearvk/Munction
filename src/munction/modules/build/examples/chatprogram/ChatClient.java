b684b39a2ec9509e7d10556e838329749f6e8187
Mr. Max Rupplin <mearvk@outlook.com>
2019-02-19 14:19:36 -0500

    [latest]

---
+++ src/munction/modules/build/examples/chatprogram/ChatClient.java
@@ -0,0 +1,8 @@
+package munction.modules.build.examples.chatprogram;
+
+import munction.modules.build.MunctionComponent;
+
+public class ChatClient extends MunctionComponent
+{
+
+}
---
+++ src/munction/modules/build/examples/chatprogram/ChatServer.java
@@ -0,0 +1,8 @@
+package munction.modules.build.examples.chatprogram;
+
+import munction.modules.build.MunctionComponent;
+
+public class ChatServer extends MunctionComponent
+{
+
+}
