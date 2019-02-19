21175c7dcd5ff7e7d5fd4ba62796c38d3c9cf08b
Mr. Max Rupplin <mearvk@outlook.com>
2019-02-19 14:21:08 -0500

    [latest]

--- src/munction/modules/build/examples/chatprogram/ChatClient.java
+++ src/munction/modules/build/examples/chatprogram/ChatClient.java
@@ -1,8 +1,11 @@
 package munction.modules.build.examples.chatprogram;
 
 import munction.modules.build.MunctionComponent;
+import munction.modules.build.UserInterfaceStrategyX;
 
 public class ChatClient extends MunctionComponent
 {
+    UserInterfaceStrategyX userinterfacestrategy = new UserInterfaceStrategyX();
+
 
 }
