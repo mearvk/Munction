21ba5db94647b5b23c5b03593b03deeb883ad370
Mr. Max Rupplin <mearvk@outlook.com>
2019-03-04 17:23:22 -0500

    [latest]

--- src/munction/localized/StandardMunctionClassloaderExtender.java
+++ src/munction/localized/StandardMunctionClassloaderExtender.java
@@ -1,12 +1,27 @@
 package munction.localized;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
 public class StandardMunctionClassloaderExtender
 {
     public void standardload(String classname)
     {
         try
         {
-            Class.forName(classname);
+            Class klass = Class.forName(classname);
+
+            for(Field field : klass.getDeclaredFields())
+            {
+                for(Annotation annotation : klass.getDeclaredAnnotations())
+                {
+                    java.lang.System.out.println(annotation);
+                }
+            }
+        }
+        catch(ClassNotFoundException cnfe)
+        {
+            java.lang.System.err.println("TBD: "+cnfe);
         }
         catch(Exception exception)
         {
